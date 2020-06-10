c6537c8bc3e4d3e573cb10ad9b71e566bc2c21e9
Леонид Арефьев <arefevleonid@gmail.com>
2020-06-10 22:00:13 +0200

    installation script update

--- zzz_install_and_then_remove_it.sh
+++ zzz_install_and_then_remove_it.sh
@@ -4,3 +4,4 @@ find ./ -type f  -exec chmod u-x+r,g-x+r,o-x+r {} \;
 find ./ -type d -exec chmod u+rx,g+rx,o+rx {} \;
 find ./ -type f -iname \*.py -exec chmod u+rx,g+rx,o+rx {} \;
 chmod u+rx,g+rx,o+rx ./zzz_install_and_then_remove_it.sh
+chmod u-x,g-x,o-x ./_common/api/*
